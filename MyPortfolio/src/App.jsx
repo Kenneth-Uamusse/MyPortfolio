425364bd0c25032acdb3770fd32046ec391b4111
Kenneth Uamusse <kennethuamusse10@gmail.com>
2025-02-13 15:47:47 +0200

    About section final stylization implemented

--- MyPortfolio/src/App.jsx
+++ MyPortfolio/src/App.jsx
@@ -1,11 +1,14 @@
+import About from "./components/About";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+
 const App = () => {
   return (
     <>
       <Header />
       <main>
         <Hero />
+        <About/>
       </main>
     </>
   );
