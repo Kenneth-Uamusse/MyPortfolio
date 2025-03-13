ba3b76ba698a00eb1ac1d40286d8682498673997
Kenneth Uamusse <kennethuamusse10@gmail.com>
2025-03-13 16:22:28 +0200

    Project refaturation v1.3.2

--- MyPortfolio/src/utils/workUtil.jsx
+++ MyPortfolio/src/utils/workUtil.jsx
@@ -1,38 +1,38 @@
 export const works = [
   {
-    imgSrc: "/images/bapp.png",
+    imgSrc: "./assets/bapp.png",
     title: "Full stack hotel booking app",
     tags: ["API", "Development"],
     projectLink:
       "https://github.com/Kenneth-Uamusse/FrontEndProjects/tree/master/Hotel-Managment",
   },
   {
-    imgSrc: "/images/iks.png",
+    imgSrc: "./assets/iks.png",
     title: "Ikarus Tech",
     tags: ["UI", "UX"],
     projectLink:
       "https://ikarustech.co.mz/",
   },
   {
-    imgSrc: "/images/sigm.png",
+    imgSrc: "./assets/sigm.png",
     title: "Microcredit Managment",
     tags: ["API", "UI"],
     
   },
   {
-    imgSrc: "/images/play4me.png",
+    imgSrc: "./assets/play4me.png",
     title: "TunePick",
     tags: ["API", "Development"],
   },
   {
-    imgSrc: "/images/ecm.png",
+    imgSrc: "./assets/ecm.png",
     title: "ECM website",
     tags: ["eCommerce", "API"],
     projectLink:
       "https://github.com/Kenneth-Uamusse/BackEndProjects/tree/master/E-commerce",
   },
   {
-    imgSrc: "/images/stapp.webp",
+    imgSrc: "./assets/stapp.webp",
     title: "Subscription Tracker",
     tags: ["API", "MVC"],
     projectLink:
